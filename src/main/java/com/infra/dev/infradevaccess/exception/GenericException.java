f81207e4e65b88c05810a79e1728f2a88cbaa76c
sgbytes-git <keerthiramraj@gmail.com>
2020-02-10 17:13:58 +0800

    Initial Commit

--- src/main/java/com/infra/dev/infradevaccess/exception/GenericException.java
+++ src/main/java/com/infra/dev/infradevaccess/exception/GenericException.java
@@ -1,8 +1,11 @@
 package com.infra.dev.infradevaccess.exception;
 
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class GenericException extends Exception{
 
-    private static final long serialversionUID= 1L;
+    private static final long serialVersionUID= 1L;
 
     public GenericException(){
         super();
